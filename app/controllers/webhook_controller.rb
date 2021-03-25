a28db64d2bbf42671a39dcd026f683425af28f88
山下秀星 <yamashitashuusei@Shuseis-MacBook-Pro.local>
2021-03-25 17:08:59 +0900

    make it better

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -54,7 +54,7 @@ class WebhookController < ApplicationController
 	        	when "応募する！"
 
 	        		uid = event['source']['userId']  #userId取得
-	        		post = Post.new(uid)
+	        		post = Post.new(user_id: uid)
 	        		post.save
 
 	        		message = {
