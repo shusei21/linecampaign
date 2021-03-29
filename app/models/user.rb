45404dbf546469d248ed3a4dc2bc640221d17eb4
山下秀星 <yamashitashuusei@Shuseis-MacBook-Pro.local>
2021-03-29 14:20:09 +0900

    make it better

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
 	attr_encrypted :user_id, key: 'This is a key that is 256 bits!!'
-	blind_index :user_id, key: Rails.application.credentials.blind_index_master_key
+	blind_index :user_id, key: ENV["blind_index_master_key"]
 	validates :user_id, uniqueness: true
 end
