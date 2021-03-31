c9bd980d87db0a5621cb0c9eb2452b849111c804
山下秀星 <yamashitashuusei@Shuseis-MacBook-Pro.local>
2021-03-31 12:39:05 +0900

    make it better

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -66,8 +66,8 @@ class WebhookController < ApplicationController
 
 		        		if user.nil?
 
-		        		#new_user = User.new(user_id: uid, campaign_flag: true)
-		        		#new_user.save
+		        		new_user = User.new(user_id: uid, campaign_flag: false)
+		        		new_user.save
 
 		        		message = {
 				          type: 'text',
