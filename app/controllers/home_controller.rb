8e8459949e45b67502b9f3660f04d6dfb0ad92b4
山下秀星 <yamashitashuusei@Shuseis-MacBook-Pro.local>
2021-04-01 14:43:46 +0900

    make it better

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,32 @@
+require 'csv'
+
 class HomeController < ApplicationController
   def top
-  	@user = User.all
-  	
+  	@users = User.all
+
+    respond_to do |format|
+      format.html
+      format.csv do |csv|
+        send_posts_csv(@posts)
+      end
+    end
   end
+
+  def send_users_csv(users)
+    csv_data = CSV.generate do |csv|
+      header = %w(id created_at updated_at user_id campaign_flag)
+      csv << header
+
+      @users.each do |user|
+        values = [user.id,user.created_at,user.updated_at,user.user_id,user.campaign_flag]
+        csv << values
+      end
+
+    end
+    send_data(csv_data, filename: "users.csv")
+  end
+
+  end
+
+
 end
