89f379d83b007fa6138e798c08fae438cf4b0eb6
山下秀星 <yamashitashuusei@Shuseis-MacBook-Pro.local>
2021-04-01 15:00:34 +0900

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#top'
+  get '/index' => 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/recieve' => 'webhook#recieve'
 end
