5a98e272563fdc02dd218d31b354f6d557c6ba8c
山下秀星 <yamashitashuusei@Shuseis-MacBook-Pro.local>
2021-04-02 17:43:57 +0900

    make it better

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,6 @@ module Linecampaign
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
+    config.autoload_paths += %W(#{config.root}/lib)
   end
 end
