edac9b668fd98739541bb256a9b082eb1e0fee59
山下秀星 <yamashitashuusei@Shuseis-MacBook-Pro.local>
2021-04-09 10:28:12 +0900

    make it better

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -187,7 +187,7 @@ class WebhookController < ApplicationController
 			          text: "ご不明な点がございましたか？\n\nお問い合わせは下記URLから$\nhttps://ask.step.rakuten.co.jp/inquiry-form/index.phtml?shop_id=253896&act=input&ms=500&fidmmy=1",
 			          emojis: [
 			          		  {
-							    "index": 13,
+							    "index": 30,
 							    "productId": "5ac1bfd5040ab15980c9b435",
 							    "emojiId": "009"
 							  }
